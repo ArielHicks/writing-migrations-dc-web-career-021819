00f0787a41389346267e55c23e40da2f2f687c95
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-28 20:02:56 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -3,7 +3,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
   add_column :students do |t|
    t.integer :grade
-   t.string :birthdate
+   t.datetime :birthdate
    end
   end
 end
